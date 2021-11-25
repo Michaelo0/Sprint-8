eb1e4cef38aacc3ab6fa3fe9f140862302652b42
Vincent Marillion <vmarilli@student.21-school.ru>
2021-11-25 20:53:29 +0300

    rabbitmq task

--- message-system/src/main/kotlin/com/example/retailer/RetailerApplication.kt
+++ message-system/src/main/kotlin/com/example/retailer/RetailerApplication.kt
@@ -1,10 +1,46 @@
 package com.example.retailer
 
+import com.example.retailer.adapter.ConsumerImpl
+import com.example.retailer.adapter.DistributorPublisher
+import com.example.retailer.adapter.DistributorPublisherImpl
+import com.rabbitmq.client.Consumer
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class RetailerApplication
+{
+
+	@Bean
+	fun queue(): Queue {
+		return Queue("retailer_queue", false)
+	}
+
+	@Bean
+	fun exchange(): TopicExchange {
+		return TopicExchange("distributor_exchange")
+	}
+
+	@Bean
+	fun binding(queue: Queue, exchange: TopicExchange): Binding? {
+		return BindingBuilder.bind(queue).to(exchange).with("retailer.Michaelo0.#")
+	}
+
+	@Bean
+	fun receiver(): ConsumerImpl {
+		return ConsumerImpl()
+	}
+
+	@Bean
+	fun sender(): DistributorPublisher {
+		return DistributorPublisherImpl()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<RetailerApplication>(*args)
